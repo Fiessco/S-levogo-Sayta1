0d1dce411c580e7180dbdb0d45f4240bae55397c
Fiessco <ledaschevnikita@gmail.com>
2022-12-11 20:40:56 +0300

    Добавлен проект

---
+++ src/main/java/com/example/S/levogo/Sayta/SLevogoSaytaApplication.java
@@ -0,0 +1,13 @@
+package com.example.S.levogo.Sayta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLevogoSaytaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SLevogoSaytaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/S/levogo/Sayta/SLevogoSaytaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.S.levogo.Sayta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SLevogoSaytaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
